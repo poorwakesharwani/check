90ad9b1261b84ff8c42eeaf07a5519edf825506a
poorwa1 <poorwa@cloudshope.com>
2020-09-25 15:49:56 +0530

    Signed-off-by: poorwa1 <poorwa@cloudshope.com>

--- signupuser.php
+++ signupuser.php
@@ -10,12 +10,12 @@
 <?php
 
 extract($_POST);
-include("database.php");
+include("config.php");
 $rs=mysqli_query($conn,"select * from emp where user_id='$user_id'");
 if (mysqli_num_rows($rs)>0)
 {
 	echo "<br><br><br><div class=head1>Login Id Already Exists</div>";
-	echo "<br><div class=head1><a href=login.php>Login</a></div>";
+	echo "<br><div class=head1><a href=index.php>Login</a></div>";
 	exit;
 }
 $sql = "INSERT INTO emp(user_id,login,pass,name,email)
@@ -24,7 +24,7 @@ VALUES ('$user_id','$login','$pass','$name','$email')";
 if ($conn->query($sql) === TRUE) {
   echo "<br><br><br><div class=head1>Your Login ID  $user_id Created Sucessfully</div>";
   echo "<br><div class=head1>Please Login using your Login ID</div>";
-   echo "<br><div class=head1><a href=login.php>Login</a></div>";
+   echo "<br><div class=head1><a href=index.php>Login</a></div>";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
